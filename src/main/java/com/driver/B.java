f9c7ef430334968059ca2d79972c3d560356e799
CodeWithPrateekk <pru151998@gmail.com>
2023-06-13 14:39:42 +0530

    again

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -4,8 +4,14 @@ public class B extends A{
 
 
 
-    @Override
+
     String meth(){
         return "Method is overridden in Extendend class B";
+    }
+
+    public static void main(String args[]) {
+        B obj = new B();
+        obj.meth();
+
     }
 }
