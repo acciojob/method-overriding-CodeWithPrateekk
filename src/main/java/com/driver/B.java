2d2c294e1e88df3bb0369e411c906a3453b49c88
CodeWithPrateekk <pru151998@gmail.com>
2023-07-01 16:25:45 +0530

    assi

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -4,8 +4,11 @@ public class B extends A{
 
 
 
+
     @Override
     String meth(){
+
+       // System.out.println("Method is overridden in Extendend class B");
         return "Method is overridden in Extendend class B";
     }
 
