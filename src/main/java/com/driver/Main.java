2c05999926c157ec06c867736f401bc3ccbd4f3d
CodeWithPrateekk <pru151998@gmail.com>
2023-07-01 17:03:00 +0530

    aaaaaaaaaaaaaaaaa

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
 
     public static void main(String args[]) {
-        B obj = new B();
-        obj.meth();
+        B object = new B();
+        object.meth();
 
     }
 
